da89f2cbe1511a42f1d249d08f37763b62b14f54
sunjinkang <2060037942@qq.com>
2023-03-24 17:47:41 +0800

    change to ast for get node

---
+++ project/filter_project_locale/index.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  console.log('这是测试用的中文');
+};
+
+export default Test;
--- project/scripts/getNotLocaleFileAndLineNumber.js
+++ project/scripts/getNotLocaleFileAndLineNumber.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const process = require('process');
+const traverse = require('@babel/traverse');
+const parser = require('@babel/parser');
 
 const getDirAllFile = (dir) => {
   const allFile = [];
@@ -46,34 +48,58 @@ for (const key of fileSet) {
     }
   }
 }
-fs.appendFileSync(pathFile, allFiles.join('\n'), 'utf8');
+fs.appendFileSync(pathFile, allFiles.join('\n'));
+
+const detectChinese = (text) => {
+  return /[\u4e00-\u9fa5]/.test(text);
+};
 
 const file = fs.readFileSync(pathFile, 'utf8');
 const fileArr = file.split(/\r?\n/);
 fileArr.forEach((fileName) => {
   const current = fs.statSync(fileName);
   if (current.isFile() === true) {
-    const lineFile = fs.readFileSync(fileName, 'utf8');
-    const arr = lineFile.split(/\r?\n/);
-    arr.forEach((line, index) => {
-      var match =
-        !line.includes('//') && !line.includes('<!--') && !line.includes('*')
-          ? line.match(/[\u4e00-\u9faf]+/g)
-          : '';
-      if (!!match) {
-        const localeContent = fs.existsSync(localeFile)
-          ? fs.readFileSync(localeFile, 'utf8')
-          : '';
-        let data = '';
-        if (localeContent.includes(fileName)) {
-          data = `\nline ${index + 1}: ${match.join('、')}`;
-        } else {
-          data = `${fs.existsSync(localeFile) ? '\n\n' : ''}${fileName}\nline ${
-            index + 1
-          }: ${match.join('、')}`;
-        }
-        fs.appendFileSync(localeFile, data, 'utf8');
-      }
+    const lineFile = fs.readFileSync(fileName).toString();
+
+    const code = parser.parse(lineFile, {
+      sourceType: 'module',
+      // plugins: ['json', 'html', 'js'],
+      plugins: ['jsx', 'typescript', 'decorators-legacy', 'syntax-import-meta'],
     });
+    traverse.default(code, {
+      Property(pa) {
+        console.log(pa.node.key);
+        console.log(pa.node.value);
+        const test = detectChinese(pa.node.key.name || pa.node.value.value);
+        if (test) {
+          const info = `${pa.node.key.name || pa.node.value.value}\nline: ${
+            pa.node.loc.start.line
+          }`;
+          fs.appendFileSync(localeFile, info);
+        }
+      },
+    });
+
+    // const arr = lineFile.split(/\r?\n/);
+    // arr.forEach((line, index) => {
+    //   var match =
+    //     !line.includes('//') && !line.includes('<!--') && !line.includes('*')
+    //       ? line.match(/[\u4e00-\u9faf]+/g)
+    //       : '';
+    //   if (!!match) {
+    //     const localeContent = fs.existsSync(localeFile)
+    //       ? fs.readFileSync(localeFile)
+    //       : '';
+    //     let data = '';
+    //     if (localeContent.includes(fileName)) {
+    //       data = `\nline ${index + 1}: ${match.join('、')}`;
+    //     } else {
+    //       data = `${fs.existsSync(localeFile) ? '\n\n' : ''}${fileName}\nline ${
+    //         index + 1
+    //       }: ${match.join('、')}`;
+    //     }
+    //     fs.appendFileSync(localeFile, data);
+    //   }
+    // });
   }
 });
